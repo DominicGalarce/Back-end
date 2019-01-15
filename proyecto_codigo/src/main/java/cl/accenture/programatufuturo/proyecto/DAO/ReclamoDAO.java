83e2392fb50e78e1c6ab4258822c12b453c7ec1e
DominicGalarce <domi.galarce@gmail.com>
2019-01-15 01:18:16 -0300

    avance metodo reclamoDAO

--- proyecto_codigo/src/main/java/cl/accenture/programatufuturo/proyecto/DAO/ReclamoDAO.java
+++ proyecto_codigo/src/main/java/cl/accenture/programatufuturo/proyecto/DAO/ReclamoDAO.java
@@ -4,10 +4,8 @@ import cl.accenture.programatufuturo.proyecto.exception.SinConexionException;
 import cl.accenture.programatufuturo.proyecto.model.Reclamo;
 import cl.accenture.programatufuturo.proyecto.model.Estado;
 
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 
 public class ReclamoDAO {
 
@@ -29,6 +27,59 @@ public class ReclamoDAO {
             pstatementInsert.setDate(4, (Date) a.getFecha());
             pstatementInsert.setInt(5, a.getEstado().getId());
             pstatementInsert.setString(6,a.getRegion());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void buscarPorId(int a) throws SinConexionException{
+        try {
+
+            PreparedStatement pstatementSelect = conexion.getConexion().prepareStatement("SELECT * FROM reclamo WHERE idreclamo=?");
+            pstatementSelect.setInt(1, a);
+            ResultSet rs = pstatementSelect.executeQuery();
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("idreclamo"));
+                System.out.println(rs.getString("titulo"));
+                System.out.println(rs.getString("descripcion"));
+
+                PreparedStatement psstatementSelect = conexion.getConexion().prepareStatement("SELECT nombre FROM usuario WHERE idusuario in (SELECT idreclamo FROM reclamo WHERE idreclamo=?");
+                psstatementSelect.setInt(1, a);
+                ResultSet rss = pstatementSelect.executeQuery();
+                System.out.println(rss.getString("nombre"));
+
+                System.out.println(rs.getDate("fecha"));
+                System.out.println(rs.getString("region"));
+
+                PreparedStatement ppsstatementSelect = conexion.getConexion().prepareStatement("SELECT nombre FROM estado WHERE idestado in (SELECT idreclamo FROM reclamo WHERE idreclamo=?");
+                ppsstatementSelect.setInt(1, a);
+                ResultSet rrss = pstatementSelect.executeQuery();
+                System.out.println(rrss.getString("nombre"));
+
+                PreparedStatement pesstatementSelect = conexion.getConexion().prepareStatement("SELECT nombre FROM tiporeclamo WHERE idtiporeclamo in (SELECT idreclamo FROM reclamo WHERE idreclamo=?");
+                pesstatementSelect.setInt(1, a);
+                ResultSet ress = pstatementSelect.executeQuery();
+                System.out.println(ress.getString("nombre"));
+
+            }
+
+            PreparedStatement prstatementSelect = conexion.getConexion().prepareStatement("SELECT * FROM respuesta WHERE id=?");
+            prstatementSelect.setInt(1, a);
+            ResultSet prs = prstatementSelect.executeQuery();
+
+            while (prs.next()) {
+                System.out.println(prs.getString("comentario"));
+
+                PreparedStatement ppesstatementSelect = conexion.getConexion().prepareStatement("SELECT nombre FROM usuario WHERE idusuario in (SELECT idreclamo FROM reclamo WHERE idreclamo=?");
+                ppesstatementSelect.setInt(1, a);
+                ResultSet rress = pstatementSelect.executeQuery();
+                System.out.println(rress.getString("nombre"));
+
+                System.out.println(prs.getDate("fecha"));
+
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
